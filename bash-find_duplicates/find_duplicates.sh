155381985803697dbe865df55b53f192018ab376
elratmacfat <elratmacfat@gmx.de>
2024-02-18 15:52:07 +0100

    Seems to be working

--- bash-find_duplicates/find_duplicates.sh
+++ bash-find_duplicates/find_duplicates.sh
@@ -1,36 +1,93 @@
 #!/bin/bash
 
+
 files=$(find . -iname "*.txt" -or -iname "*.md")
 
-declare -A hashtable
+# A white-space must not be interpreted as delimiting character,
+# because there could be files and directories containing spaces.
+# We're assuming here that there are no new-line characters
+#
+IFS_backup=$IFS
+IFS_newline_only=$'\n'
+IFS=$IFS_newline_only
 
-for file in $files
-do
-	checksum=$(dd status=none if=$file bs=100 count=1 | md5sum | sed 's/[^a-f0-9]//g')
-	if [[ ${#checksum} -ne 32 ]];
-	then
-		echo "Something went wrong. Could not get MD5 checksum for $file." 
-		exit
-	fi
+# Associative array that is used to map each file to the MD5-checksum 
+# that's generated from the file's first 100 bytes.
+# Multiple files being mapped to the same checksum are considered 
+# possible duplicates.
+#
+declare -A possible_duplicates
+declare -A actual_duplicates
 
-	echo "$checksum: $file"
-	
+echo "----------------------------------------------------"
+echo "Partially calculating MD5 checksums for all files..."
+echo "----------------------------------------------------"
+
+for file in $files; do
+
+	# md5sum produces a string starting with the 32 byte wide MD5 sum followed
+	# by some additional information, separated by spaces
+	#
+	IFS=$IFS_backup
+	partial_checksum=($(dd status=none if="${file}" bs=100 count=1 | md5sum))
+	IFS=$IFS_newline_only
+
+	# Append current filename. If it is the first filename to be mapped
+	# to the partial checksum the delimiter is omitted.
+	#
 	unset delimiter
-	if [[ ${hashtable[$checksum]} ]]
-	then 
-		delimiter=":"; 
+	if [[ ${possible_duplicates[$partial_checksum]} ]]; then 
+		delimiter=${IFS};
 	fi
-	hashtable[$checksum]+="${delimiter}${file}"
-	echo ${hashtable[$checksum]}
-
+	possible_duplicates[$partial_checksum]+="${delimiter}${file}"
 done
 
-echo "---------------------------------------------"
 
-echo "Number of redundant files: ${#hashtable[@]}"
+echo "--------------------------------------------------------"
+echo "Calculating MD5 checksums for all possible duplicates..."
+echo "--------------------------------------------------------"
 
-for key in "${!hashtable[@]}";
+for key in "${!possible_duplicates[@]}"
 do
-	echo $key
-	echo "${hashtable[$key]}" | sed 's/^/\t/g' | sed 's/:/\n\t/g'
+	number_of_possible_duplicates=0
+	for dummy in ${possible_duplicates[$key]}; do ((++number_of_possible_duplicates)); done
+
+	if [[ $number_of_possible_duplicates -gt 1 ]]
+	then
+		#echo "$key: $number_of_possible_duplicates"
+		
+		for file in ${possible_duplicates[$key]};
+		do
+			IFS=$IFS_backup
+			full_checksum=($(md5sum "$file"))
+			IFS=$IFS_newline_only
+
+			echo "    $full_checksum : $file"
+
+
+			unset delimiter
+			if [[ ${actual_duplicates[$full_checksum]} ]]; then 
+				delimiter=${IFS};
+			fi
+			actual_duplicates[$full_checksum]+="${delimiter}${file}"
+		done
+	fi
+done
+
+echo "-------"
+echo "Results"
+echo "-------"
+
+for key in "${!actual_duplicates[@]}"
+do
+	number_of_actual_duplicates=0
+	for dummy in ${actual_duplicates[$key]}; do ((++number_of_actual_duplicates)); done
+	if [[ $number_of_actual_duplicates -gt 1 ]]
+	then
+		echo "$key: $number_of_actual_duplicates"
+		for file in ${actual_duplicates[$key]};
+		do
+			echo "    $file"
+		done
+	fi
 done
